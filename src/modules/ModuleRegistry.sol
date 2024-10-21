ca2cf9e27c1e1b32de63b924623deaa57a8b7efb
pheuberger <philipp@pheuberger.com>
2024-10-21 15:37:31 +0700

    feat: add AccessControl to module registry
    We need to be able to own this contract as protocol operators in the
    beginning.

--- src/modules/ModuleRegistry.sol
+++ src/modules/ModuleRegistry.sol
@@ -1,16 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ModuleRegistry {
+import {AccessControl} from "openzeppelin/contracts/access/AccessControl.sol";
+
 error EmptyModuleName();
+
+contract ModuleRegistry is AccessControl {
+    bytes32 public constant MODULE_MANAGER_ROLE = keccak256("MODULE_MANAGER_ROLE");
 
     mapping(string => address) public modules;
 
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MODULE_MANAGER_ROLE, msg.sender);
+    }
 
     function register(
         string calldata name,
         address moduleAddress
-    ) external {
+    ) external onlyRole(MODULE_MANAGER_ROLE) {
         if (bytes(name).length == 0) {
             revert EmptyModuleName();
         }
